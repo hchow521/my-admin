15675019b92ce59f7a24e7a9f7f36f2d725baf8a
hchow <hchow521@163.com>
2019-08-11 23:21:46 +0800

    搭建vue项目 新建page/index页面 增加css-loader包 增加rem.js

---
+++ src/page/index.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="index">
+        <img src="../assets/logo.png" alt="">
+        <h2>{{welcome}}</h2>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "index",
+        data () {
+            return {
+                welcome: '欢迎访问hchow'
+            }
+        }
+    }
+</script>
+
+<style scoped lang="less">
+    .index {
+        text-align: center;
+        color: #2c3e50;
+        margin-top: 1rem;
+        h2 {
+            font-size: .18rem;
+        }
+    }
+</style>
